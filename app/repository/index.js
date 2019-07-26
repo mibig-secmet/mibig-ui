9606223d230a505090959187e861e2b08be9521c
Kai Blin <kblin@biosustain.dtu.dk>
2019-07-26 13:12:12 +0200

    repository: Load repository entries sequentially for faster page loads
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- app/repository/index.js
+++ app/repository/index.js
@@ -1,12 +1,13 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import tooltip from 'angular-ui-bootstrap/src/tooltip';
+import angularInview from 'angular-inview';
 
 import routes from './repository.routes';
 import RepositoryService from './repository.service';
 import repository from './repository.component';
 
-export default angular.module('mibig.repository', [uiRouter, tooltip])
+export default angular.module('mibig.repository', [uiRouter, tooltip, angularInview])
   .component('mibigRepository', repository)
   .service('RepositoryService', RepositoryService)
   .config(routes)
--- app/repository/repository.component.js
+++ app/repository/repository.component.js
@@ -1,4 +1,20 @@
+const STEPS = 200;
+
 class RepositoryCtrl {
+  constructor() {
+    this.limit = STEPS;
+  }
+
+  loadMore(last, inview) {
+    if (last && inview) {
+      this.limit += STEPS;
+    }
+  }
+
+  resetLimit() {
+    this.limit = STEPS;
+  }
+
   showEntry(entry) {
     window.open('/go/' + entry.accession, '_blank');
   }
