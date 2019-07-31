bcdc3589f46b56a79a6f71506c3bac4f07baac66
Kai Blin <kblin@biosustain.dtu.dk>
2019-07-31 09:47:05 +0200

    query: Enable more categories
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- app/query/builder/term/category.service.js
+++ app/query/builder/term/category.service.js
@@ -2,18 +2,17 @@ export default class CategoryService{
 	constructor(){
 		this.categories = [
 			{val: '', desc: '--- Select a category ---'},
-//			{val: 'accession', desc: 'MIBiG accession'},
+			{val: 'acc', desc: 'MIBiG accession'},
 			{val: 'type', desc: 'BGC type',},
 			{val: 'compound', desc: 'Compound name',},
 //			{val: 'minimal', desc: 'Entries with minimal annotations', category: 'Quality filters'},
-//			{val: 'strain', desc: 'Strain', category: 'Taxonomy'},
-//			{val: 'species', desc: 'Species', category: 'Taxonomy'},
-//			{val: 'genus', desc: 'Genus', category: 'Taxonomy'},
-//			{val: 'family', desc: 'Family', category: 'Taxonomy'},
-//			{val: 'order', desc: 'Order', category: 'Taxonomy'},
-//			{val: 'class', desc: 'Class', category: 'Taxonomy'},
-//			{val: 'phylum', desc: 'Phylum', category: 'Taxonomy'},
-//			{val: 'superkingdom', desc: 'Superkingdom', category: 'Taxonomy'},
+			{val: 'species', desc: 'Species', category: 'Taxonomy'},
+			{val: 'genus', desc: 'Genus', category: 'Taxonomy'},
+			{val: 'family', desc: 'Family', category: 'Taxonomy'},
+			{val: 'order', desc: 'Order', category: 'Taxonomy'},
+			{val: 'class', desc: 'Class', category: 'Taxonomy'},
+			{val: 'phylum', desc: 'Phylum', category: 'Taxonomy'},
+			{val: 'superkingdom', desc: 'Superkingdom', category: 'Taxonomy'},
 		];
 	};
 }
